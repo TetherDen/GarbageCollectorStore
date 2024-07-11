67889c50a5a08344153715160a3278a45ee1d86d
Andy <134444257+Just-Andy404@users.noreply.github.com>
2024-07-11 23:50:06 +0300

    Update User.cs

--- Users/User.cs
+++ Users/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,6 @@ namespace GarbageCollectorStore
         public Guid Id { get; } = Guid.NewGuid();
         public string Login { get; set; }
         public string Password { get; set; }
-
         protected User(string login, string password)
         {
             Login = login;
