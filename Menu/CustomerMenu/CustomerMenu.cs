c5a990ef7575ec753486c934702a195628dc6f9d
Andy <134444257+Just-Andy404@users.noreply.github.com>
2024-07-11 23:49:14 +0300

    Update CustomerMenu.cs

--- Menu/CustomerMenu/CustomerMenu.cs
+++ Menu/CustomerMenu/CustomerMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using GarbageCollectorStore.Users.Customer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,9 +16,10 @@ namespace GarbageCollectorStore
                 Console.WriteLine(TextColor.MenuText("Customer Menu:"));
                 Console.WriteLine(TextColor.MenuText("1. View Products"));
                 Console.WriteLine(TextColor.MenuText("2. Add Product to Cart"));
-                Console.WriteLine(TextColor.MenuText("3. View Cart"));
-                Console.WriteLine(TextColor.MenuText("4. Checkout"));
-                Console.WriteLine(TextColor.MenuText("5. Leave a Review"));
+                Console.WriteLine(TextColor.MenuText("3. Remove product from the cart"));
+                Console.WriteLine(TextColor.MenuText("4. View Cart"));
+                Console.WriteLine(TextColor.MenuText("5. Checkout"));
+                Console.WriteLine(TextColor.MenuText("6. Leave a Review"));
                 Console.WriteLine(TextColor.MenuText("0. Logout"));
                 string choice = Console.ReadLine();
                 switch (choice)
@@ -26,15 +28,28 @@ namespace GarbageCollectorStore
                         ProductManager.ShowList();
                         break;
                     case "2":
-                        // TODO: AddProductToCart();
+                        (UserManager.CurrentUser as Customer).Cart.AddProductToCart();
                         break;
                     case "3":
-                        // TODO: ViewCart();
+                        if (UserManager.CurrentUser is Customer)
+                        {
+                            int index;
+                            Console.WriteLine(TextColor.RequestText("Enter ID of the product to remove:"));
+                            int.TryParse(Console.ReadLine(), out index);
+                            (UserManager.CurrentUser as Customer).Cart.RemoveProductFromCart(index);
+                        }
+                        else
+                        {
+                            Console.WriteLine(TextColor.ErrorText("Invalid input. Please enter a valid ID."));
+                        }
                         break;
                     case "4":
-                        // TODO: Checkout();
+                        (UserManager.CurrentUser as Customer).Cart.ViewCart();
                         break;
                     case "5":
+                        (UserManager.CurrentUser as Customer).Cart.Checkout();
+                        break;
+                    case "6":
                         Review.LeaveReview();
                         break;
                     case "0":
